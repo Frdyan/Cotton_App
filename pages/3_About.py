40f3278cc632fcca7f138b4f7c3fd6cfaa0428e0
Frdyan <ferdiharyadi123@gmail.com>
2024-07-23 19:35:19 +0700

    More Content

--- pages/3_About.py
+++ pages/3_About.py
@@ -2,30 +2,30 @@ import streamlit as st
 
 st.title("Tentang Aplikasi")
 st.write("""
-Aplikasi ini dirancang untuk kebutuhan penulisan ilmiah dan bertujuan untuk membantu dalam klasifikasi penyakit daun kapas. 
-Aplikasi ini menggunakan model pembelajaran mendalam yang telah dilatih untuk mengenali berbagai penyakit pada daun kapas berdasarkan gambar yang diunggah oleh pengguna.
+Aplikasi ini dirancang untuk kebutuhan penulisan ilmiah dan bertujuan untuk membantu dalam klasifikasi daun kapas. 
+Aplikasi ini menggunakan model pembelajaran mendalam yang telah dilatih untuk mengenali berbagai jenis pada daun kapas berdasarkan gambar yang diunggah oleh pengguna.
 
 **Fitur Utama:**
-- **Klasifikasi Penyakit Daun Kapas**: Aplikasi ini mampu mengklasifikasikan empat kategori utama penyakit daun kapas:
+- **Klasifikasi Penyakit Daun Kapas**: Aplikasi ini mampu mengklasifikasikan empat kategori utama daun kapas:
   1. Bacterial Blight
   2. Curl Virus
   3. Fusarium Wilt
   4. Healthy
 
-- **Deskripsi Penyakit**: Aplikasi ini juga menyediakan deskripsi rinci mengenai setiap penyakit, termasuk penyebab, gejala, dan metode pengendalian.
+- **Deskripsi Penyakit**: Aplikasi ini juga menyediakan deskripsi rinci mengenai setiap penyakit , termasuk penyebab, gejala,  metode pengendalian dan deskripsi penyakit daun yang sehat.
 
 **Cara Menggunakan Aplikasi:**
 1. **Halaman Utama**: 
    - Muat gambar daun kapas melalui fungsi unggah gambar.
    - Aplikasi akan memproses gambar dan memberikan prediksi mengenai penyakit yang mungkin ada pada daun tersebut.
-   - Deskripsi singkat mengenai penyakit yang terdeteksi akan ditampilkan.
+   - Deskripsi singkat mengenai daun yang terdeteksi akan ditampilkan.
 
 2. **Halaman Deskripsi**:
-   - Pengguna dapat membaca deskripsi lebih rinci mengenai setiap penyakit.
-   - Deskripsi termasuk gambar contoh, penyebab, gejala, dan metode pengendalian.
+   - Pengguna dapat membaca deskripsi lebih rinci mengenai setiap jenis daun kapas.
+   - Deskripsi termasuk gambar contoh, penyebab dan gejala.
 
-**Informasi Tambahan:**
-- **Ferdiansyah Haryadi**
+
+- **Dibuat Oleh Ferdiansyah Haryadi**
 - **Universitas Gunadarma**
 
 Untuk informasi lebih lanjut, kunjungi tautan di bawah ini:
